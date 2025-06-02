f5136793d2c770589ca629bb529b716b67fde030
Mehul Uttam <mehulsavio@gmail.com>
2025-06-03 03:02:06 +0530

    Add Space Grotesk font and update layout styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,13 @@ import Dither from "./utils/background/_Dither/Dither";
 const Home = () => {
   return (
     <div className="h-screen w-screen relative">
-      <div className="absolute inset-0">
+      <div className="absolute inset-0 z-10">
         <Dither />
       </div>
-      <div className="relative z-10 h-[10rem] w-full flex items-center justify-center">
-        <div className="w-1/2 h-1/2 bg-white/10 backdrop-blur-sm rounded-lg p-4">
+      <div className="relative h-full w-full flex items-center justify-center p-20">
+        <div className="w-1/2 h-[10rem] z-20 bg-white/10 backdrop-blur-sm rounded-lg p-4">
           <h1 className="text-2xl font-bold">Hello</h1>
-          <p className="text-sm text-gray-500">
+          <p className="text-xl text-white">
             I'm a software engineer with a passion for building web
             applications.
           </p>
