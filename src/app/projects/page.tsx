48f412342559b03016df3c5921c18552afc76966
Mehul Uttam <mehulsavio@gmail.com>
2025-06-21 01:59:16 +0530

    Update SVG styling in Projects component for improved responsiveness and visual consistency

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -22,7 +22,7 @@ export default function Projects() {
             viewBox="0 0 446 30"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
-            className="absolute md:top-14 left-0 bottom-0 right-0 top-6 "
+            className="absolute top-4 md:top-14 left-0 bottom-0 right-0 w-32 md:w-auto h-4 md:h-auto"
           >
             <motion.path
               initial={{ pathLength: 0, opacity: 0 }}
@@ -30,9 +30,10 @@ export default function Projects() {
               transition={{ duration: 3, ease: "easeInOut" }}
               d="M2 27C2 27 29.2929 3.23735 50.5 2.49996C71.7071 1.76257 85.4833 29.2121 103.5 27C121.517 24.7878 124.877 3.52701 143 2.49996C161.123 1.47291 175.365 31.0148 188 27C200.635 22.9851 198.548 5.32938 211.5 2.49996C224.452 -0.32946 233.589 28.85 248 27C262.411 25.15 261.625 4.61612 276 2.49996C290.375 0.383809 300.61 29.751 314 27C327.39 24.249 325.781 5.97989 339 2.49996C352.219 -0.979964 368.102 30.0932 382 27C395.898 23.9067 394.863 4.19582 409 2.49996C423.137 0.80411 443.5 27 443.5 27"
               stroke="#F60004"
-              strokeWidth="6"
+              strokeWidth="3"
               strokeLinecap="round"
               strokeLinejoin="round"
+              className="md:stroke-[6px]"
             />
           </svg>
         </span>
